e6e7018f1021a7ed3f2f300576f6fca73bdce5dd
ArtFacchinetti <arthurfacchinetti10@gmail.com>
2025-08-18 21:21:56 -0300

    Firts commit

---
+++ aula2/atividade.ts
@@ -0,0 +1,43 @@
+import csv from './lertext.ts'
+const ignore:Array<string> = ["de", "da", "dos", "do"]
+
+function csvParser (csv:string):Object {
+    const nameArray:Array<string> = csv.split(";");
+
+    return nameArray
+} 
+
+function format(string:string):string {
+    const lower:string = string.toLowerCase()
+    const lowerTrim:string = lower.trim()
+
+    if (testIgnore(lowerTrim) == 1 ){
+        console.log('Não ignorado')
+        const firstToUpper:string = lowerTrim.charAt(0).toUpperCase() + lowerTrim.slice(1)
+
+        return firstToUpper
+    }else {
+        return lowerTrim
+    }
+}
+
+function testIgnore(str:string):string | number {
+    const foundWord = ignore.find((ignoredWord: string) => {
+        return str === ignoredWord;
+    });
+
+    return 1
+}
+
+// function formatArray(array:string[]):string {
+//     let virtual:string[] = []
+//     array.forEach(name => {
+//         const words:string[] = name.split(" ")
+//         words.forEach(word => {
+//             format(word)
+//         })
+//         virtual.
+//     });
+// }
+
+console.log(csv)
