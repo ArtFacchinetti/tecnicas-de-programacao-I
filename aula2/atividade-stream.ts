b8498ee2b7adbc8b87e4e8f0c7034247ee94e6cb
ArtFacchinetti <arthurfacchinetti10@gmail.com>
2025-08-29 22:52:14 -0300

    ReadStream funcionando com cálculo de performance

--- aula2/atividade-stream.ts
+++ aula2/atividade-stream.ts
@@ -1,49 +1,65 @@
-/*
-Nomes em maiúsculo no csv
-
-1: Receber dados
-2: Tratar capitalizando (menos preposições)
-3: Reescrever num csv
-
-
-Soluções:
-Criar uma stream de transformação, que lê o csv, trata, e devolve para outro
-
-Para ler o csv cmo objeto, posso utilizar o parse() da lib csv-parse.
-O dado entrará no tranformer, será parseado, tratato, parseado para csv, gravado na sáida pelo trasformer
-
-Pera... Nem preciso de parser. O trasformer pode ler linha por linha
-
-Para salvar num outro arquivo
-*/
-
-import { transform } from "typescript"
-
-//USANDO TRANSFORMER (NÃO SEI SE É NECESSÁRIO)
-// Lê com readstream, passa esses dados para o transformer com pipe, passa esse resultado para uma createwritestream com pipe tbm
-
 const fs = require('fs')
-const {Transform} = require('stream')
+const { Transform } = require('stream')
 
-function niceUpperCase (chunk:Buffer) {
-  const lowerCaseChunk = chunk.toString().toLowerCase();
-  const firstLetter = lowerCaseChunk.charAt(0).toLocaleUpperCase();
-  const treatedChunk = firstLetter + lowerCaseChunk.substring(1)
+const ignoredWords = ['de', 'da', 'das', 'do', 'dos', 'e']
 
-  console.log(chunk.toString())
+const PATH = './nomes.csv';
 
-  return treatedChunk
+function niceUpperCase(chunk: Array<string>) {
+  const lowerCaseChunkArray = chunk
+    .toString()
+    .toLocaleLowerCase()
+    .split('\r\n')
+    .map((completeName) => {
+      return completeName
+        .trim()
+        .split(' ')
+        .map(n => {
+          if (ignoredWords.includes(n)) {
+            return n
+          } else {
+            return n.charAt(0).toLocaleUpperCase() + n.substring(1).trim()
+          }
+        })
+        .join(' ')
+    })
+    .join('\r\n')
+
+  return lowerCaseChunkArray
 }
 
 const upperCaseTransformer = new Transform({
-  transform(chunk:Buffer, encoding:any, callback:any){
+  transform(chunk: Array<string>, encoding: any, callback: any) {
     this.push(niceUpperCase(chunk));
     callback();
   }
 });
 
-fs.createReadStream('./mock.csv')
-.pipe(upperCaseTransformer)
-.pipe(fs.createWriteStream('./result.csv'))
 
-// NÃO FUNCIONA POR QUE O CHUNK NÃO É LINHA POR LINHA, USAR READLINE NA PRÓXIMA
+
+async function execute() {
+  const writeCSV = () => {
+    return new Promise((resolve, reject) => {
+      fs.createReadStream(PATH)
+        .pipe(upperCaseTransformer)
+        .pipe(fs.createWriteStream('./result.csv'))
+        .on('finish', () => { 
+          resolve("ok");
+        })
+        .on('error', (err:Error) => reject(err));
+    });
+  };
+
+  console.log('Tratamento iniciado...');
+
+  const start = performance.now();
+
+  const retorno = await writeCSV(); 
+
+  const end = performance.now();
+  const timeToFinish = end - start;
+
+  console.log(`Tratamento finalizado! \n -- Demorou ${Math.floor(timeToFinish) / 1000} segundos ;)`);
+}
+
+execute();
